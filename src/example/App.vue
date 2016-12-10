c6857f27967cf7743e0c9220f21415de3a8aeb7e
Jingxian Mao <jessemao@gmail.com>
2016-12-10 15:13:59 -0800

    add missing example links to app.

--- src/example/App.vue
+++ src/example/App.vue
@@ -1,11 +1,14 @@
 <template>
   <div class="page">
     <div class="page__header">
-      <h1 class="header__title">Vue-WeUI</h1>
-      <p class="header__desc">Vue.js for WeUI</p>
+      <h1 class="header__title">VeUI</h1>
+      <p class="header__desc">Vue.js 2.0 for WeUI</p>
     </div>
     <div class="page__body">
-      <grid v-bind:data="gridData"></grid>
+      <div v-for="cate in categories">
+        <p class="subtitle">{{cate.title}}</p>
+        <grid :data="cate.items"></grid>
+      </div>
     </div>
     <router-view></router-view>
   </div>
@@ -16,51 +19,136 @@ import Grid from 'components/grid'
 export default {
   data () {
     return {
-      gridData: [
+      categories: [
         {
-          text: 'ActionSheet',
-          url: 'actionsheet',
-          image: '/static/images/icon_nav_actionSheet.png'
+          title: '表单',
+          items: [
+            {
+              text: 'Button',
+              url: 'button',
+              image: '/static/images/icon_nav_button.png'
+            },
+            {
+              text: 'Input',
+              url: 'input',
+              image: '/static/images/icon_nav_cell.png'
+            },
+            {
+              text: 'Slider',
+              url: 'slider',
+              image: '/static/images/icon_nav_progress.png'
+            },
+            {
+              text: 'Uploader',
+              url: 'uploader',
+              image: '/static/images/icon_nav_form.png'
+            }
+          ]
         },
         {
-          text: 'Button',
-          url: 'button',
-          image: '/static/images/icon_nav_button.png'
+          title: '基础组件',
+          items: [
+            {
+              text: 'Article',
+              url: 'article',
+              image: '/static/images/icon_nav_article.png'
+            },
+            {
+              text: 'Badge',
+              url: 'badge',
+              image: '/static/images/icon_nav_button.png'
+            },
+            {
+              text: 'Footer',
+              url: 'footer',
+              image: '/static/images/icon_nav_button.png'
+            },
+            {
+              text: 'Gallery',
+              url: 'gallery',
+              image: '/static/images/icon_nav_icons.png'
+            },
+            {
+              text: 'Grid',
+              url: '/',
+              image: '/static/images/icon_nav_layout.png'
+            },
+            {
+              text: 'Icons',
+              url: 'icon',
+              image: '/static/images/icon_nav_icons.png'
+            },
+            {
+              text: 'Loadmore',
+              url: 'loadmore',
+              image: '/static/images/icon_nav_flow.png'
+            },
+            {
+              text: 'Panel',
+              url: 'panel',
+              image: '/static/images/icon_nav_panel.png'
+            },
+            {
+              text: 'Preview',
+              url: 'preview',
+              image: '/static/images/icon_nav_panel.png'
+            },
+            {
+              text: 'Progress',
+              url: 'progress',
+              image: '/static/images/icon_nav_progress.png'
+            }
+          ]
         },
         {
-          text: 'Cell',
-          url: 'cell',
-          image: '/static/images/icon_nav_cell.png'
+          title: '操作反馈',
+          items: [
+            {
+              text: 'Actionsheet',
+              url: 'actionsheet',
+              image: '/static/images/icon_nav_actionsheet.png'
+            },
+            {
+              text: 'Dialog',
+              url: 'dialog',
+              image: '/static/images/icon_nav_dialog.png'
+            },
+            {
+              text: 'Message',
+              url: 'message',
+              image: '/static/images/icon_nav_msg.png'
+            },
+            {
+              text: 'Toast',
+              url: 'toast',
+              image: '/static/images/icon_nav_toast.png'
+            }
+          ]
         },
         {
-          text: 'Dialog',
-          url: 'dialog',
-          image: '/static/images/icon_nav_dialog.png'
+          title: '导航相关',
+          items: [
+            {
+              text: 'Navbar',
+              url: 'navbar',
+              image: '/static/images/icon_nav_nav.png'
+            },
+            {
+              text: 'Tabbar',
+              url: 'tabbar',
+              image: '/static/images/icon_nav_tab.png'
+            }
+          ]
         },
         {
-          text: 'Article',
-          url: 'article',
-          image: '/static/images/icon_nav_article.png'
-        },
-        {
-          text: 'Message',
-          url: 'message',
-          image: '/static/images/icon_nav_msg.png'
-        },
-        {
-          text: 'Progress',
-          url: 'progress',
-          image: '/static/images/icon_nav_progress.png'
-        },
-        {
-          text: 'Toast',
-          url: 'toast',
-          image: '/static/images/icon_nav_toast.png'
-        },
-        {
-          text: 'Form',
-          url: 'form',
-          image: '/static/images/icon_nav_form.png'
+          title: '搜索相关',
+          items: [
+            {
+              text: 'SearchBar',
+              url: 'searchbar',
+              image: '/static/images/icon_nav_search_bar.png'
+            }
+          ]
         }
       ]
     }
@@ -70,7 +158,11 @@ export default {
   }
 }
 </script>
-
 <style>
-
+.subtitle {
+  padding: 5px;
+  color: #3cc51f;
+  font-size: 15px;
+  text-align: center;
+}
 </style>
